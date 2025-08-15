b8f38af845c8fc1b090ad8c122a2ba1c6fc98baf
Muhammed Ali Öztürk <Muhammed_Ali95@outlook.com.tr>
2025-08-15 11:08:01 +0300

    Update kayıt.py

--- Biometric-ML/kayıt.py
+++ Biometric-ML/kayıt.py
@@ -2,28 +2,19 @@ import os
 
 def rename_files_in_directory(directory, start_number):
     try:
-        # Klasördeki tüm dosyaları listele
         files = os.listdir(directory)
-        files.sort()  # Dosya sıralamasını garanti etmek için
-
-        # Dosyaları yeniden adlandır
+        files.sort() 
         current_number = start_number
         for i, file_name in enumerate(files):
-            # Yalnızca ".wav" uzantılı dosyaları seç
             if file_name.endswith(".wav"):
                 old_path = os.path.join(directory, file_name)
                 new_name = f"{current_number}.wav"
                 new_path = os.path.join(directory, new_name)
-
                 os.rename(old_path, new_path)
                 print(f"{old_path} -> {new_path}")
-
                 current_number += 1
-
         print("Dosya yeniden adlandırma işlemi tamamlandı!")
     except Exception as e:
         print(f"Hata oluştu: {e}")
 
-
-# Fonksiyonu çağır
 rename_files_in_directory("kayit",72)
